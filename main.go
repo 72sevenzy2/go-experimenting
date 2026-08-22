af7b81168d2341a9ff16870e171c7825805bc714
72 <72sevenzy2@gmail.com>
2026-08-22 12:09:57 +0500

    test singleflight pattern (concurrency).

--- main.go
+++ main.go
@@ -1,31 +1,52 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"time"
+	"sync"
 )
 
-func worker(ctx context.Context) {
-	for {
-		select {
-		case <-ctx.Done(): // wait for cancel() func to stop the worker
-			fmt.Println("stopped working")
-			return  // exits before the printing happens so we need to schedule some time for print to execute.
+type Call struct {
+	err    error
+	data   any
+	waiter sync.WaitGroup
+}
 
-		case <-time.After(1 * time.Second): // wait 1 second after each print msg:
-			fmt.Println("working..")
-		}
+type board struct {
+	argsGuard sync.Mutex
+	args      map[string]*Call
+}
+
+func NewBoard() *board {
+	return &board{
+		args: make(map[string]*Call),
 	}
 }
 
-func main() {
-	ctx, cancel := context.WithCancel(context.Background()) // initialising the context cancellation
+// singleflight concurrency pattern
+func (b *board) GetUser(user string) any {
+	b.argsGuard.Lock()
+	if user, ok := b.args[user]; ok {
+		b.argsGuard.Unlock()
+		user.waiter.Wait()
+		return user.data
+	}
 
-	go worker(ctx) // start the worker
+	c := new(Call)
+	c.waiter.Add(1)
+	b.args[user] = c
+	b.argsGuard.Unlock()
 
-	time.Sleep(3 * time.Second) // wait 3 seconds after calling the cancel() func
-	cancel()
+	c.data = expensiveCalc()
+	c.err = nil
+	c.waiter.Done()
 
-	time.Sleep(500 * time.Millisecond) // give some time for the "stopped working" msg to be printed
+	b.argsGuard.Lock()
+	delete(b.args, user)
+	b.argsGuard.Unlock()
+
+	return c.data
+}
+
+func expensiveCalc() string {
+	// some time-consuming calculation
+	return "some expensive data"
 }
